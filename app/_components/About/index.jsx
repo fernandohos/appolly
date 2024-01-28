43a544a808e3b2c7a589c492d2ef9f9e26b822f0
Fernando Henrique <fernandohenriqueosantos@hotmail.com>
2024-01-27 21:07:01 -0300

    :sparkles: add about section

---
+++ app/_components/About/index.jsx
@@ -0,0 +1,53 @@
+import styles from "./styles.module.css";
+import aboutPhone from "@public/about-phone.png";
+import { Card } from "./Card";
+import Image from "next/image";
+
+const aboutCardInfo = [
+  {
+    title: "CREATIVE DESIGN",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.",
+  },
+  {
+    title: "EASY TO USE",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.",
+  },
+  {
+    title: "BEST USER EXPERIENCE",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus amet etiam tincidunt rhoncus, ullamcorper velit.",
+  },
+];
+
+export function About() {
+  return (
+    <div className={styles.aboutContainer} id="about">
+      <div>
+        <h2 className={styles.title}>About our app</h2>
+        <p className={styles.description}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Viverra nunc
+          ante velit vitae. Est tellus vitae, nullam lobortis enim. Faucibus
+          amet etiam tincidunt rhoncus, ullamcorper velit. Ullamcorper risus
+          tempor, ac nunc libero urna, feugiat.
+        </p>
+      </div>
+      <div className={styles.content}>
+        <div className={styles.image}>
+          <Image
+            src={aboutPhone}
+            alt="phone with banking app open showing investment charts"
+            sizes="386px 526px"
+            fill
+          />
+        </div>
+        <div>
+          {aboutCardInfo.map((cardInfo) => (
+            <Card key={cardInfo.title} {...cardInfo} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,14 @@
 import styles from "./page.module.css";
 import { Header } from "@components/Header";
 import { Hero } from "@components/Hero";
+import { About } from "./_components/About";
 
 export default function Home() {
   return (
     <main>
       <Header />
       <Hero />
+      <About />
       hello world
     </main>
   );
